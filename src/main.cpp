3d35c27ddd3c6a427c74ba8532348ca9868bd4f6
Tom Irgang <thomas@irgang.eu>
2023-04-16 15:53:50 +0200

    move functions to ESblinkt

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,18 @@
 #include <Arduino.h>
 #include <NeoPixelBus.h>
+#include <ESblinkt.hpp>
 
-const uint16_t PixelCount = 179;
-const uint8_t PixelPin = 7;
+#define SATURATION 128
+#define PIXEL_COUNT 179
+#define PIXEL_PIN 23
 
-const uint16_t EndRight = 53;
-const uint16_t EndTop = 125;
+NeoPixelBus<NeoGrbFeature, NeoWs2812xMethod> strip(PIXEL_COUNT, PIXEL_PIN);
+struct strip leds;
 
-#define colorSaturation 128
-
-    NeoPixelBus<NeoGrbFeature, NeoWs2812xMethod>
-        strip(PixelCount, PixelPin);
-
-RgbColor red(colorSaturation, 0, 0);
-RgbColor green(0, colorSaturation, 0);
-RgbColor blue(0, 0, colorSaturation);
-RgbColor white(colorSaturation);
+RgbColor red(SATURATION, 0, 0);
+RgbColor green(0, SATURATION, 0);
+RgbColor blue(0, 0, SATURATION);
+RgbColor white(SATURATION);
 RgbColor black(0);
 
 enum MODE
@@ -28,23 +25,35 @@ enum MODE
 enum MODE mode = THREE_SIDED;
 
 uint32_t rainbow_speed = 1;
+struct three_sided three_sided_config;
 RgbColor solid_color = red;
-RgbColor three_sided_colors[3] = {red, green, blue};
-bool three_sided_anim = true;
-uint32_t three_sided_speed = 500;
 
-RgbColor color_wheel(uint8_t pos);
-void fill_rainbow(uint8_t offset);
-void fill_solid(RgbColor color);
-void rainbow();
-void solid();
-void three_sided();
+void setPixelColor(uint16_t pixel, Neo3ByteElements::ColorObject color)
+{
+  strip.SetPixelColor(pixel, color);
+}
 
+void commit(void)
+{
+  strip.Show();
+}
 
 void setup()
 {
+  leds.pixelCount = PIXEL_COUNT;
+  leds.setPixelColor = &setPixelColor;
+  leds.commit = &commit;
+
+  three_sided_config.endRight = 53;
+  three_sided_config.endTop = 125;
+  three_sided_config.colors[0] = red;
+  three_sided_config.colors[1] = green;
+  three_sided_config.colors[2] = blue;
+  three_sided_config.anim = true;
+  three_sided_config.speed = 500;
+
   Serial.begin(115200);
-  
+
   // this resets all the neopixels to an off state
   strip.Begin();
   strip.Show();
@@ -58,98 +67,16 @@ void loop()
   switch (mode)
   {
   case RAINBOW:
-    rainbow();
+    rainbow(leds);
     break;
   case SOLID:
-    solid();
+    solid(solid_color, leds);
     break;
   case THREE_SIDED:
-    three_sided();
+    three_sided(three_sided_config, leds);
     break;
   default:
     Serial.printf("Invalid mode! Mode: %d\n", mode);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
-}
-
-RgbColor color_wheel(uint8_t pos)
-{
-  uint16_t p = (uint8_t)pos;
-
-  if (p < 85)
-  {
-    return RgbColor((uint8_t)(255 - p * 3), (uint8_t)(p * 3), (uint8_t)0);
-  }
-  else if (p < 170)
-  {
-    p -= 85;
-    return RgbColor((uint8_t)0, (uint8_t)(255 - p * 3), (uint8_t)(p * 3));
-  }
-  else
-  {
-    p -= 170;
-    return RgbColor((uint8_t)(p * 3), (uint8_t)0, (uint8_t)(255 - p * 3));
-  }
-}
-
-void fill_rainbow(uint8_t offset) {
-  uint16_t color_index = 0;
-  for (uint16_t i = 0; i < PixelCount; i++)
-  {
-    color_index = (i + (uint16_t)offset) % 256;
-    strip.SetPixelColor(i, color_wheel(color_index));
-  }
-}
-
-void fill_solid(RgbColor color) {
-  for (uint16_t i = 0; i<PixelCount; i++) {
-    strip.SetPixelColor(i, color);
-  }
-}
-
-void rainbow()
-{
-  for (int i = 0; i < 256; i++)
-  {
-    fill_rainbow(i);
-    strip.Show();
-    vTaskDelay(rainbow_speed / portTICK_PERIOD_MS);
-  }
-}
-
-void solid()
-{
-  fill_solid(solid_color);
-  strip.Show();
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-}
-
-void three_sided()
-{
-  uint32_t delay = 1000;
-  uint8_t n = 1;
-  
-  if (three_sided_anim) {
-    delay = three_sided_speed;
-    n = 3;
-  }
-
-  for (int off = 0; off < n; off++)
-  {
-
-    for (int j = 0; j < EndRight; j++)
-    {
-      strip.SetPixelColor(j, three_sided_colors[(off) % 3]);
-    }
-    for (int j = EndRight; j < EndTop; j++)
-    {
-      strip.SetPixelColor(j, three_sided_colors[(off + 1) % 3]);
-    }
-    for (int j = EndTop; j < PixelCount; j++)
-    {
-      strip.SetPixelColor(j, three_sided_colors[(off + 2) % 3]);
-    }
-    strip.Show();
-    vTaskDelay(delay / portTICK_PERIOD_MS); 
   }
 }
